7f127d78ff020acf35d6a0056d4346ba274f8868
neyneyalldayday <drfrankenstein123@gmail.com>
2023-09-24 15:00:59 -0500

    starting this api stuffs

---
+++ app/weather/weather.ts
@@ -0,0 +1,24 @@
+import { fetchLocationData } from "./location";
+import  type { LocationInfo } from "./location";
+
+const GEOCODE_API_URL = "https://geocode.maps.co/search";
+
+async function main(): Promise<number>{
+    if (process.argv.length !== 3){
+        console.error("useage: weather LOCATION")
+        return 1;
+    }
+    const location = process.argv[2];
+    let locationInfo: LocationInfo;
+    try {
+        locationInfo = await fetchLocationData(GEOCODE_API_URL, location);
+    } catch (error) {
+       console.error(error) 
+       return 1;
+    }
+
+    console.log(locationInfo)
+return await Promise.resolve(0);
+}
+
+main().catch((err) => console.error(err))
